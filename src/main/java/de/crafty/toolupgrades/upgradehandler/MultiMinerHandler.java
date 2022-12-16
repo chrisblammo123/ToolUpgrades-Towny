b3f0b5360356b8a73a649ae4010a5421ca44c4fe
Chris Struck <chrisblammo123@gmail.com>
2022-12-16 04:34:59 -0600

    Added first pass for the Multi Miner Handler, logging will most likely change to add town info.
    Should go to mc console but I did not check beforehand so im gonna have to tinker a lot, especially considering this is untested and there is no test server set up atm.

--- src/main/java/de/crafty/toolupgrades/upgradehandler/MultiMinerHandler.java
+++ src/main/java/de/crafty/toolupgrades/upgradehandler/MultiMinerHandler.java
@@ -12,7 +12,8 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
 
-//
+// Towny API Import
+import com.palmergames.bukkit.towny.TownyAPI;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,89 +22,98 @@ public class MultiMinerHandler implements Listener {
 
 
 
-    private final List<ItemStack> currentlyInUse = new ArrayList<>();
+	private final List<ItemStack> currentlyInUse = new ArrayList<>();
 
-    @EventHandler
-    public void onBlockBreak(BlockBreakEvent event) {
+	@EventHandler
+	public void onBlockBreak(BlockBreakEvent event) {
 
-        Player player = event.getPlayer();
-        ItemStack usedStack = player.getInventory().getItemInMainHand();
+		Player player = event.getPlayer();
+		ItemStack usedStack = player.getInventory().getItemInMainHand();
 
-        if (!ToolManager.hasUpgrade(usedStack, ToolUpgrade.MULTI_MINER) || player.isSneaking() || currentlyInUse.contains(usedStack))
-            return;
-
-
-        List<Block> located = this.locateBlocks(event.getBlock(), ToolUpgrades.getInstance().multiMinerMaxBlocks());
+		if (!ToolManager.hasUpgrade(usedStack, ToolUpgrade.MULTI_MINER) || player.isSneaking() || currentlyInUse.contains(usedStack))
+			return;
 
 
-        currentlyInUse.add(usedStack);
-        located.forEach(b -> {
-
-            if(usedStack.getType() == Material.AIR)
-                return;
-
-                //checking if a player can build/destyroy somewhere
-                // boolean bBuild = PlayerCacheUtil.getCachePermission(player, block.getLocation(), block.getType(), TownyPermission.ActionType.BUILD);
+		List<Block> located = this.locateBlocks(event.getBlock(), ToolUpgrades.getInstance().multiMinerMaxBlocks());
 
 
-            // if player is in town/has perms, break block
-            player.breakBlock(b);
-        });
+		currentlyInUse.add(usedStack);
+		located.forEach(b -> {
 
-        currentlyInUse.remove(usedStack);
-    }
+			if(usedStack.getType() == Material.AIR)
+				return;
+
+				//checking if a player can build/destyroy somewhere
+				// boolean bBuild = PlayerCacheUtil.getCachePermission(player, block.getLocation(), block.getType(), TownyPermission.ActionType.BUILD);
+			if (PlayerCacheUtil.getCachePermission(player, b.getLocation(), b.getType(), TownyPermission.ActionType.DESTROY)) {
+				
+				// Logging for testing purposes
+				console.log(String.format("%s is breaking block at %s, of type %s", player.getDisplayName(), b.getLocation().toString(), b.getType()));
+				
+				// if player is in town/has perms, break block
+				player.breakBlock(b);
+			}
+			else {
+				// Log the failure
+				console.log(String.format("%s failed to break block at %s, of type %s", player.getDisplayName(), b.getLocation().toString(), b.getType()))
+			}
+
+		});
+
+		currentlyInUse.remove(usedStack);
+	}
 
 
-    private List<Block> locateBlocks(Block src, int max) {
-        List<Block> blocks = this.getAttachedBlocks(src);
-        List<Block> tmpBlocks = new ArrayList<>(blocks);
+	private List<Block> locateBlocks(Block src, int max) {
+		List<Block> blocks = this.getAttachedBlocks(src);
+		List<Block> tmpBlocks = new ArrayList<>(blocks);
 
-        while (tmpBlocks.size() > 0) {
+		while (tmpBlocks.size() > 0) {
 
-            List<Block> currentBlocks = new ArrayList<>(tmpBlocks);
-            tmpBlocks.clear();
+			List<Block> currentBlocks = new ArrayList<>(tmpBlocks);
+			tmpBlocks.clear();
 
-            currentBlocks.forEach(block -> {
+			currentBlocks.forEach(block -> {
 
 
-                List<Block> attached = this.getAttachedBlocks(block).stream().filter(b -> !blocks.contains(b) && !b.equals(src)).toList();
-                for (Block b : attached) {
-                    if (blocks.size() >= max)
-                        break;
+				List<Block> attached = this.getAttachedBlocks(block).stream().filter(b -> !blocks.contains(b) && !b.equals(src)).toList();
+				for (Block b : attached) {
+					if (blocks.size() >= max)
+						break;
 
-                    blocks.add(b);
-                    tmpBlocks.add(b);
-                }
-            });
+					blocks.add(b);
+					tmpBlocks.add(b);
+				}
+			});
 
-        }
+		}
 
-        return blocks;
-    }
+		return blocks;
+	}
 
-    private List<Block> getAttachedBlocks(Block src) {
+	private List<Block> getAttachedBlocks(Block src) {
 
-        List<Block> list = new ArrayList<>();
+		List<Block> list = new ArrayList<>();
 
-        World world = src.getWorld();
+		World world = src.getWorld();
 
-        for (int x = -1; x <= 1; x++) {
-            for (int z = -1; z <= 1; z++) {
-                for (int y = -1; y <= 1; y++) {
+		for (int x = -1; x <= 1; x++) {
+			for (int z = -1; z <= 1; z++) {
+				for (int y = -1; y <= 1; y++) {
 
-                    if (x == 0 && y == 0 && z == 0)
-                        continue;
+					if (x == 0 && y == 0 && z == 0)
+						continue;
 
-                    Block block = world.getBlockAt(src.getX() + x, src.getY() + y, src.getZ() + z);
-                    if (block.getType() == src.getType())
-                        list.add(block);
+					Block block = world.getBlockAt(src.getX() + x, src.getY() + y, src.getZ() + z);
+					if (block.getType() == src.getType())
+						list.add(block);
 
-                }
-            }
-        }
+				}
+			}
+		}
 
-        return list;
+		return list;
 
-    }
+	}
 
 }
